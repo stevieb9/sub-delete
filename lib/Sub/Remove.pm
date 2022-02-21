400fac1af6dacb447bd43b0769b701afe7d16f46
Steve Bertrand <steveb@cpan.org>
2022-02-21 11:19:47 -0800

    Added initial POD and code

--- lib/Sub/Remove.pm
+++ lib/Sub/Remove.pm
@@ -7,12 +7,56 @@ our $VERSION = '0.01';
 
 sub __placeholder {}
 
+sub sub_remove {
+    my ($sub_name, $class) = @_;
+
+    my $src;
+
+    # get the calling package symbol table name
+    {
+        no strict 'refs';
+        $src = \%{ $class . '::' };
+    }
+
+    # loop through all symbols in calling package, looking for subs
+    for my $symbol ( keys %$src ) {
+        # get all code references, make sure they're valid
+        my $sub = *{ $src->{$symbol} }{CODE};
+        next unless defined $sub and defined &$sub;
+
+        # save all other slots of the typeglob
+        my @slots;
+
+        for my $slot (qw( SCALAR ARRAY HASH IO FORMAT )) {
+                my $elem = *{ $src->{$symbol} }{$slot};
+                next unless defined $elem;
+                push @slots, $elem;
+        }
+
+        # clear out the source glob
+        undef $src->{$symbol};
+
+        # replace the sub in the source
+        if ($symbol ne $sub_name) {
+            $src->{$symbol} = sub {
+                my @args = @_;
+                return $sub->(@_);
+            };
+        }
+
+        # replace the other slot elements
+        for my $elem (@slots) {
+                $src->{$symbol} = $elem;
+        }
+    }
+}
+
 1;
 __END__
 
 =head1 NAME
 
-Sub::Remove - One line description
+Sub::Remove - Remove a subroutine from the symbol table and its associated CODE glob
 
 =for html
 <a href="https://github.com/stevieb9/sub-remove/actions"><img src="https://github.com/stevieb9/sub-remove/workflows/CI/badge.svg"/></a>
@@ -21,21 +65,38 @@ Sub::Remove - One line description
 
 =head1 SYNOPSIS
 
+    use Sub::Remove qw(sub_remove);
+
+    # In a different class
+
+    sub_remove('sub_name', 'My::Class);
+
+    # In main
+
+    sub_remove('sub_name');
+
 =head1 DESCRIPTION
+
+Removes a subroutine (function or method) from the namespace.
 
 =head1 METHODS
 
-=head2 name
+=head2 sub_remove($sub_name, $class)
 
-Description.
+Removes a sub and all traces to it.
 
 I<Parameters>:
 
-    $bar
+    $sub_name
 
-I<Mandatory, String>: The name of the thing with the guy and the place.
+I<Mandatory, String>: The name of the subroutine to remove.
 
-I<Returns>: C<0> upon success.
+    $class
+
+I<Optional, String>: The name of the class to remove the symbol from.
+Defaults to C<main> if not sent in.
+
+I<Returns>: True (C<1>) upon success.
 
 =head1 AUTHOR
 
